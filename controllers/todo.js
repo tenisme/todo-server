e27561c48b5e06a0e2bb85e851a10f856cbbb571
tenisme <iamchoma@gmail.com>
2020-08-12 11:02:37 +0900

    update 200812-1102 create api

--- controllers/todo.js
+++ controllers/todo.js
@@ -14,6 +14,7 @@ exports.getAllTodo = async (req, res, next) => {
   let limit = req.query.limit;
 
   if (!offset || !limit) {
+    console.log("error : insert offset and limit");
     res.status(400).json({ message: "파라미터 셋팅 에러" });
     return;
   }
@@ -22,6 +23,7 @@ exports.getAllTodo = async (req, res, next) => {
   let limitIsNum = validator.isNumeric(String(limit));
 
   if (!offsetIsNum || !limitIsNum) {
+    console.log("error : offset and limit isn't number");
     res.status(400).json({
       success: false,
       message: "offset 혹은 limit에는 숫자만 입력이 가능합니다",
@@ -30,6 +32,7 @@ exports.getAllTodo = async (req, res, next) => {
   }
 
   if (limit <= 0) {
+    console.log("error : insert number upper 1");
     res
       .status(400)
       .json({ success: false, message: "1 이상의 숫자를 입력해야 합니다" });
@@ -44,15 +47,104 @@ exports.getAllTodo = async (req, res, next) => {
   try {
     [rows] = await connection.query(query);
   } catch (e) {
+    console.log("error : " + e);
     res.status(500).json({ success: false, message: `DB ERROR`, error: e });
     return;
   }
+
+  res.status(200).json({ success: true, cnt: rows.length, items: rows });
 };
 
-// @desc    할일 완료 여부 체크 api
-// @route   POST /api/v1/todo
+// @desc    할일 완료 체크 api
+// @route   PUT /api/v1/todo/check
 // @req     todo_id
-// @res     success, result
+// @res     success
 exports.checkTodo = async (req, res, next) => {
   console.log(chalk.bold("<<  Check Todo API executed  >>"));
+
+  let todo_id = req.body.todo_id;
+
+  if (!todo_id) {
+    console.log("error : not found todo_id");
+    res.status(400).json({ message: "todo_id 입력 필수" });
+    return;
+  }
+
+  let todoIdIsNum = validator.isNumeric(String(todo_id));
+
+  if (!todoIdIsNum) {
+    console.log("error : todo_id isn't number");
+    res.status(400).json({
+      success: false,
+      message: "todo_id에는 숫자만 입력이 가능함",
+    });
+    return;
+  }
+
+  todo_id = Number(todo_id);
+
+  query = `update todo set completed = true where id = ${todo_id}`;
+
+  try {
+    [result] = await connection.query(query);
+
+    if (result.affectedRows == 0) {
+      console.log("error : isn't affectedRows");
+      res.status(500).json({ success: false, error: e });
+      return;
+    }
+  } catch (e) {
+    console.log("error : " + e);
+    res.status(500).json({ success: false, message: `DB ERROR`, error: e });
+    return;
+  }
+
+  res.status(200).json({ success: true });
+};
+
+// @desc    할일 완료 취소 api
+// @route   PUT /api/v1/todo/uncheck
+// @req     todo_id
+// @res     success
+exports.unCheckTodo = async (req, res, next) => {
+  console.log(chalk.bold("<<  Uncheck Todo API executed  >>"));
+
+  let todo_id = req.body.todo_id;
+
+  if (!todo_id) {
+    console.log("error : not found todo_id");
+    res.status(400).json({ message: "todo_id 입력 필수" });
+    return;
+  }
+
+  let todoIdIsNum = validator.isNumeric(String(todo_id));
+
+  if (!todoIdIsNum) {
+    console.log("error : todo_id isn't number");
+    res.status(400).json({
+      success: false,
+      message: "todo_id에는 숫자만 입력이 가능함",
+    });
+    return;
+  }
+
+  todo_id = Number(todo_id);
+
+  query = `update todo set completed = false where id = ${todo_id}`;
+
+  try {
+    [result] = await connection.query(query);
+
+    if (result.affectedRows == 0) {
+      console.log("error : isn't affectedRows");
+      res.status(500).json({ success: false, error: e });
+      return;
+    }
+  } catch (e) {
+    console.log("error : " + e);
+    res.status(500).json({ success: false, message: `DB ERROR`, error: e });
+    return;
+  }
+
+  res.status(200).json({ success: true });
 };
--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { checkTodo, getAllTodo } = require("../controllers/todo.js");
+const {
+  checkTodo,
+  getAllTodo,
+  unCheckTodo,
+} = require("../controllers/todo.js");
 
 const router = express.Router();
 
-router.route("/").get(getAllTodo).post(checkTodo);
+router.route("/").get(getAllTodo).put(checkTodo);
+router.route("/check").put(checkTodo);
+router.route("/uncheck").put(unCheckTodo);
 
 module.exports = router;
